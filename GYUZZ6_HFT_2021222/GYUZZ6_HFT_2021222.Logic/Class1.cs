b37fa53c44b3536abf33fff57bce8d51361f4caa
Kasza Barnabas <kaszinnyo@stud.uni-obuda.hu>
2022-03-17 09:25:29 +0100

    Projects had been added to the solution.

---
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GYUZZ6_HFT_2021222.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GYUZZ6_HFT_2021222.Test
+{
+    public class Class1
+    {
+    }
+}
