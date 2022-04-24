64c41e7e936e18dd86fd45aab4f83b1175fa03bf
Kasza Barnabas <kaszinnyo@stud.uni-obuda.hu>
2022-04-24 13:20:52 +0200

    Models has been finalized.

--- GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Car.cs
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Car.cs
@@ -1,9 +1,38 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GYUZZ6_HFT_2021222.Models
 {
-    public class Class1
+    [Table("cars")]
+    public class Car
     {
+        public Car()
+        {
 
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        [Column("model")]
+        [Required]
+        public string Model { get; set; }
+
+        [Column("price")]
+        public int? BasePrice { get; set; }
+
+        [ForeignKey(nameof(Brand))]
+        [Column("brand_id")]
+        public int BrandId { get; set; }
+
+        [NotMapped]
+        public virtual Brand Brand { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Rent> Rents { get; set; }
     }
 }
