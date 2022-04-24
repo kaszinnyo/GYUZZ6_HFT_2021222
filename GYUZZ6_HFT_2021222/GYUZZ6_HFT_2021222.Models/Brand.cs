16f7ca4fe70a18c39c9f43c30123af275c3a384f
Kasza Barnabas <kaszinnyo@stud.uni-obuda.hu>
2022-04-24 13:20:52 +0200

    Models has been finalized.

--- GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Brand.cs
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Brand.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GYUZZ6_HFT_2021222.Models
 {
-    internal class Brand
+    [Table("brands")]
+    public class Brand
     {
+        public Brand()
+        {
+
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        [Column("name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
     }
 }
--- GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Rent.cs
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Rent.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GYUZZ6_HFT_2021222.Models
 {
+    [Table("rents")]
     public class Rent
     {
+        public Rent()
+        {
+
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("carId")]
+        public int CarId { get; set; }
+
+        [StringLength(20)]
+        [Column("renterName")]
+        [Required]
+        public string RenterName { get; set; }
+
+        [Column("Date")]
+        public DateTime? Date { get; set; }
+
+        [Column("rentTime")]
+        public double? RentTime { get; set; }
+
+        [Column("rating")]
+        public double? Rating { get; set; }
+
+        [NotMapped]
+        public virtual Car Car { get; set; }
     }
 }
