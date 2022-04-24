95b6b790c18d2398c2c93d9cb0cba6f5dab066e2
Kasza Barnabas <kaszinnyo@stud.uni-obuda.hu>
2022-04-24 13:13:30 +0200

    Models added.

---
+++ GYUZZ6_HFT_2021222/GYUZZ6_HFT_2021222.Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYUZZ6_HFT_2021222.Models
+{
+    internal class Brand
+    {
+    }
+}
