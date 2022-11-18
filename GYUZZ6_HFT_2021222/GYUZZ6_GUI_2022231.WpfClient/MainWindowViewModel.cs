aba8f225648c536fc10b15bb646007728b80411f
kaszinnyo <92094949+kaszinnyo@users.noreply.github.com>
2022-11-18 09:13:55 +0100

    Wpf app added.

---
+++ GYUZZ6_HFT_2021222/GYUZZ6_GUI_2022231.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GYUZZ6_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYUZZ6_GUI_2022231.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Car> Cars { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Cars = new RestCollection<Car>("http://localhost:18131/", "car");
+        }
+    }
+}
